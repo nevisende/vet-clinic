1c4c73c3a35c5e1d7d2542753708c7db616147e2
nevisende <ekonomitakipmail@gmail.com>
2022-02-14 21:25:38 +0300

    Feat: create animals table

--- data.sql
+++ data.sql
@@ -3,3 +3,8 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+
+INSERT INTO ANIMALS VALUES(1,'Agumon','03/02/2020', 0,'1', 10.23);
+INSERT INTO ANIMALS VALUES(2,'Gabumon','11/15/2018', 2,'1', 8);
+INSERT INTO ANIMALS VALUES(3,'Pikachu','6/7/2021', 1,'0', 15.04);
+INSERT INTO ANIMALS VALUES(4,'Devimon','5/12/2017', 5,'1', 11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name = 'Luna';
+
+SELECT * FROM ANIMALS WHERE name LIKE  '%mon';
+SELECT name FROM ANIMALS WHERE date_of_birth BETWEEN '1/1/2016' AND '1/1/2019';
+SELECT name FROM ANIMALS WHERE neutered='1' AND escape_attempts < 3;
+SELECT date_of_birth FROM ANIMALS WHERE name IN ('Agumon','Pikachu');
+SELECT name, escape_attempts FROM ANIMALS WHERE weight_kg > 10.5;
+SELECT * FROM ANIMALS WHERE neutered = '1';
+SELECT * FROM ANIMALS WHERE NOT name = 'Gabumon';
+SELECT * FROM ANIMALS WHERE weight_kg > 10.4 AND weight_kg < 17.3;
