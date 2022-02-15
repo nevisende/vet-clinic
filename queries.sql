09aedb82a491c7850bf30b87fb0d5a866eb2d634
nevisende <ekonomitakipmail@gmail.com>
2022-02-16 00:02:45 +0300

    Update: query and update animals table

--- queries.sql
+++ queries.sql
@@ -10,3 +10,18 @@ SELECT name, escape_attempts FROM ANIMALS WHERE weight_kg > 10.5;
 SELECT * FROM ANIMALS WHERE neutered = '1';
 SELECT * FROM ANIMALS WHERE NOT name = 'Gabumon';
 SELECT * FROM ANIMALS WHERE weight_kg > 10.4 AND weight_kg < 17.3;
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered , MAX(escape_attempts) FROM animals;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT neutered , MAX(escape_attempts) FROM animals GROUP BY neutered;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '6/1/1990' AND '12,31,2000' GROUP BY species;
